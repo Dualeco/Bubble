49f94388fc422ca1d413d947cc9dff92cdcad548
SwapDatName <bogdanpasha2000@gmail.com>
2019-01-22 13:36:29 +0200

    Implement Login screen layout

---
+++ app/src/main/java/com/kpiroom/bubble/os/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.kpiroom.bubble.os
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.kpiroom.bubble.R
+
+class LoginActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_screen)
+    }
+}
