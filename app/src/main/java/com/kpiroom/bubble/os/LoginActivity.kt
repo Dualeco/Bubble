e0c844efa85db42962113ac0425a5d87c71efa4a
SwapDatName <bogdanpasha2000@gmail.com>
2019-01-25 23:27:11 +0200

    Added a customized ProgressLayout

--- app/src/main/java/com/kpiroom/bubble/os/LoginActivity.kt
+++ app/src/main/java/com/kpiroom/bubble/os/LoginActivity.kt
@@ -1,12 +1,51 @@
 package com.kpiroom.bubble.os
 
+import android.animation.ValueAnimator
+import android.graphics.Color
+import android.graphics.Rect
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
+import android.view.animation.AccelerateDecelerateInterpolator
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.kpiroom.bubble.R
+import kotlinx.android.synthetic.main.activity_login_screen.*
+import kotlinx.android.synthetic.main.activity_login_screen.view.*
 
-class LoginActivity: AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_screen)
+
+        emailEditText.setOnTouchListener { _, _ ->
+            scrollTo(scroll)
+            false
+        }
+
+        passwordEditText.setOnTouchListener { _, _ ->
+            scrollTo(scroll)
+            false
+        }
+
+        getStartedButton.setOnClickListener {
+            root.progress("Message")
+        }
+    }
+
+    private fun scrollTo(view: View) {
+        val rect = Rect()
+        view.getGlobalVisibleRect(rect)
+
+        ValueAnimator.ofInt(scroll.scrollY, rect.bottom + scroll.scrollY)
+            .apply {
+                addUpdateListener {
+                    scroll.scrollTo(0, it.animatedValue as Int)
+                }
+
+                interpolator = AccelerateDecelerateInterpolator()
+                start()
+            }
     }
 }
---
+++ app/src/main/java/com/kpiroom/bubble/os/ResourceExtensions.kt
@@ -0,0 +1,43 @@
+package com.kpiroom.bubble.os
+
+import android.graphics.drawable.Drawable
+import android.graphics.drawable.ShapeDrawable
+import android.text.Layout
+import android.text.StaticLayout
+import android.text.TextPaint
+import android.util.TypedValue
+import androidx.annotation.*
+import androidx.core.content.ContextCompat
+
+
+fun bindString(@StringRes res: Int): String = BubbleApp.app.getString(res)
+
+fun bindString(@StringRes res: Int, arg1: String): String = BubbleApp.app.getString(res, arg1)
+fun bindString(@StringRes res: Int, arg1: Int): String = BubbleApp.app.getString(res, arg1)
+fun bindString(@StringRes res: Int, arg1: String, arg2: String): String = BubbleApp.app.getString(res, arg1, arg2)
+
+fun bindPlural(@PluralsRes res: Int, amount: Int, param: Int = amount): String =
+    BubbleApp.app.resources.getQuantityString(res, amount, param)
+
+fun bindInt(@IntegerRes res: Int): Int = BubbleApp.app.resources.getInteger(res)
+
+fun bindDimen(@DimenRes res: Int): Float = BubbleApp.app.resources.getDimension(res)
+
+fun bindColor(@ColorRes res: Int): Int = ContextCompat.getColor(BubbleApp.app, res)
+
+fun bindDrawable(@DrawableRes res: Int): Drawable = ContextCompat.getDrawable(BubbleApp.app, res) ?: ShapeDrawable()
+
+fun dpToPx(dp: Int): Int = dpToPx(dp.toFloat())
+
+fun dpToPx(dp: Float): Int = Math.round(dp * BubbleApp.app.resources.displayMetrics.density)
+
+fun spToPx(sp: Int): Float = TypedValue.applyDimension(
+    TypedValue.COMPLEX_UNIT_SP,
+    sp.toFloat(), BubbleApp.app.resources.displayMetrics
+)
+
+fun displayMetrics() = BubbleApp.app.resources.displayMetrics
+
+fun measureHeight(text: CharSequence, textPaint: TextPaint, width: Int): Int {
+    return StaticLayout(text, textPaint, width, Layout.Alignment.ALIGN_NORMAL, 1F, 0F, true).height
+}
